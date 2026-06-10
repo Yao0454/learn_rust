fe9e26bd5476a491d81f2aa9c16a121ccad99491
Yao0454 <yaoyifeng20070524@outlook.com>
2026-06-10 20:26:44 +0800

    Update two files

--- main.cpp
+++ main.cpp
@@ -4,33 +4,35 @@
 using namespace std;
 
 int get_reverse(int x) {
-    int reverse = 0;
-    while (x) {
-        int digit = x % 10;
-        reverse = reverse * 10 + digit;
-        x /= 10;
-    }
-    return reverse;
+  int reverse = 0;
+  while (x) {
+    int digit = x % 10;
+    reverse = reverse * 10 + digit;
+    x /= 10;
+  }
+  return reverse;
 }
 
 bool is_prime(int x) {
-    for (int i = 2; i * i < x; ++i) {
-        if (x % i == 0)
-            return false;
-    }
-    return true;
+  for (int i = 2; i * i < x; ++i) {
+    if (x % i == 0)
+      return false;
+  }
+
+  // return whether a number is prime
+  return true;
 }
 
 int main() {
-    int m;
-    cin >> m;
-    for (int i = m; i >= 2; --i) {
-        if (is_prime(i) && is_prime(get_reverse(i))) {
-            cout << i << endl;
-            return 0;
-        }
+  int m;
+  cin >> m;
+  for (int i = m; i >= 2; --i) {
+    if (is_prime(i) && is_prime(get_reverse(i))) {
+      cout << i << endl;
+      return 0;
     }
+  }
 
-    cout << "no\n";
-    return 0;
+  cout << "no\n";
+  return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 fn main() {
     let m = String::from("Hello Git!!");
+    
+    let a = 10;
+    let b = 20;
+
+    println!("{a + b}");
 
     println!("{m}");
 }
