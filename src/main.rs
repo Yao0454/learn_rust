e58e1d9d11e56e49214c6763b76ef07d014badfd
Yao0454 <yaoyifeng20070524@outlook.com>
2026-06-13 12:54:26 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,39 @@
 use std::io;
 
-    println!("{m}");
+fn get_reverse(mut x: i32) -> i32 {
+    let mut reverse: i32 = 0;
+    while x > 0 {
+        let digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x /= 10;
+    }
+    reverse
+}
+
+fn is_prime(x: i32) -> bool {
+    let i = 2;
+    while i * i < x {
+        if x % i == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn main() {
+    let mut m = String::new();
+    io::stdin()
+        .read_line(&mut m)
+        .expect("Failed to read a line!");
+
+    let m: i32 = m.parse().expect("Please enter a number \n[Error]: ");
+
+    for i in m..2 {
+        if is_prime(i) && is_prime(get_reverse(i)) {
+            println!("{i}");
+            return;
+        }
+    }
+
+    println!("no");
 }
